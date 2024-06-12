dc4439d48db9979adbee2043b8b9c55fb3b9d135
nandu1-k <121676407+nandu1-k@users.noreply.github.com>
2024-06-12 15:42:18 +0530

    n

--- js/main.js
+++ js/main.js
@@ -134,5 +134,36 @@ $('[data-toggle="counter-up"]').counterUp({
     delay: 10,
     time: 2000
 });
+// Skills
+$('.skill').waypoint(function () {
+    $('.progress .progress-bar').each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+}, {offset: '80%'});
+
+ // Project carousel
+ $(".project-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 25,
+    loop: true,
+    nav: false,
+    dots: true,
+    dotsData: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        },
+        1200:{
+            items:4
+        }
+    }
+});
 
 
